24b12a3be02e45d9262cd989e2947583da49311c
AYO-OLUWA-09 <folorunsoayooluwa22@gmail.com>
2025-11-05 17:40:21 +0100

    (Relational Operators)

---
+++ week-6/practice_9/target/debug/deps/practice_9.d
@@ -0,0 +1,5 @@
+C:\Users\ISMS Students\Documents\AYOOLUWA\ayooluwa.folorunsoCOS101\week-6\practice_9\target\debug\deps\practice_9.d: src\main.rs
+
+C:\Users\ISMS Students\Documents\AYOOLUWA\ayooluwa.folorunsoCOS101\week-6\practice_9\target\debug\deps\practice_9.exe: src\main.rs
+
+src\main.rs:
---
+++ week-6/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+C:\Users\ISMS\ Students\Documents\AYOOLUWA\ayooluwa.folorunsoCOS101\week-6\practice_9\target\debug\practice_9.exe: C:\Users\ISMS\ Students\Documents\AYOOLUWA\ayooluwa.folorunsoCOS101\week-6\practice_9\src\main.rs
