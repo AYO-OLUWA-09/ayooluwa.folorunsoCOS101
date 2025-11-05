ecc3ce037d38d3d4744d10efafdb72094a3137da
AYO-OLUWA-09 <folorunsoayooluwa22@gmail.com>
2025-11-05 17:45:17 +0100

    (Logical operators)

---
+++ week-6/practice_10/target/debug/deps/practice_10.d
@@ -0,0 +1,5 @@
+C:\Users\ISMS Students\Documents\AYOOLUWA\ayooluwa.folorunsoCOS101\week-6\practice_10\target\debug\deps\practice_10.d: src\main.rs
+
+C:\Users\ISMS Students\Documents\AYOOLUWA\ayooluwa.folorunsoCOS101\week-6\practice_10\target\debug\deps\practice_10.exe: src\main.rs
+
+src\main.rs:
---
+++ week-6/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\ISMS\ Students\Documents\AYOOLUWA\ayooluwa.folorunsoCOS101\week-6\practice_10\target\debug\practice_10.exe: C:\Users\ISMS\ Students\Documents\AYOOLUWA\ayooluwa.folorunsoCOS101\week-6\practice_10\src\main.rs
