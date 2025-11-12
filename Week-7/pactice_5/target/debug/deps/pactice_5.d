a240f88eee55440e89622aa62d27236e555440db
unknown <folorunsoayooluwa22@gmail.com>
2025-11-12 15:39:44 +0100

    (Refrences)

---
+++ Week-7/pactice_5/target/debug/deps/pactice_5.d
@@ -0,0 +1,5 @@
+C:\Users\HP\Documents\ayooluwa.folorunsoCOS101\Week-7\pactice_5\target\debug\deps\pactice_5.d: src\main.rs
+
+C:\Users\HP\Documents\ayooluwa.folorunsoCOS101\Week-7\pactice_5\target\debug\deps\pactice_5.exe: src\main.rs
+
+src\main.rs:
---
+++ Week-7/pactice_5/target/debug/pactice_5.d
@@ -0,0 +1,1 @@
+C:\Users\HP\Documents\ayooluwa.folorunsoCOS101\Week-7\pactice_5\target\debug\pactice_5.exe: C:\Users\HP\Documents\ayooluwa.folorunsoCOS101\Week-7\pactice_5\src\main.rs
